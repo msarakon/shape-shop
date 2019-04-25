56b57fc6e9d2f896bcfedd348a6ee0c4a071b221
Minna Sarakontu <minna.sarakontu@gmail.com>
2019-04-25 18:48:56 +0300

    updated dependencies

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       inject: false,
       template: require('html-webpack-template'),
