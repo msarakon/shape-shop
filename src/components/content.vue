eb080d85259635cc00cbdac4672d7511217606a5
Minna Sarakontu <minna.sarakontu@gmail.com>
2019-04-27 09:21:42 +0300

    initial product list

--- src/components/content.vue
+++ src/components/content.vue
@@ -1,15 +1,43 @@
 <template>
     <div class="content">
-        Hello world!
+        <div class="product-list">
+            <product
+                v-for="product in products"
+                :key="product.id"
+                :product="product" />
+        </div>
     </div>
 </template>
 
 <script>
+    import Product from './product.vue';
+
     export default {
-        name: 'AppContent'
+        name: 'AppContent',
+        components: {
+            Product
+        },
+        data() {
+            return {
+                products: []
+            }
+        }
     }
 </script>
 
 <style lang="scss">
     @import '../styles/_colors.scss';
+
+    .content {
+        padding-top: 2em;
+    }
+
+    .product-list {
+        margin: 0 auto;
+        width: 100%;
+        max-width: 800px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
 </style>
---
+++ src/components/product.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="product">
+        <div class="product-image" />
+        <div class="product-center">
+            <div class="product-name">
+                {{ product.name }}
+            </div>
+            <div class="product-description">
+                {{ product.description }}
+            </div>
+        </div>
+        <div class="product-right">
+            <div class="product-price">
+                {{ product.price }}
+            </div>
+            <div class="product-add-to-cart">
+                ADD TO CART
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Product',
+        props: {
+            product: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import '../styles/_colors.scss';
+</style>
